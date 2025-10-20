f1788ba2879f03920ceaf30441bb675db8e8ea4d
RouAn <75302395+RouAn0903@users.noreply.github.com>
2025-10-20 15:42:44 +0800

    add description
    Add comment to clarify binary conversion process.

--- LvC/odd_position.py
+++ LvC/odd_position.py
@@ -1,7 +1,7 @@
 n = eval(input())
 for i in range(n):
     num = eval(input())
-    b = bin(num)[2:]
+    b = bin(num)[2:] # 會產生如:'0b1011'
     d = b.count('1')
     if d % 2 == 1:
         print(f"{num}: YES")
